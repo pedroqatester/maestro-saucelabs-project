a3369dbefe5eccb0dad1728ca0da6419e2a81a23
Pedroqatester <pedro.henrique.pereirag12@gmail.com>
2026-09-13 18:48:42 -0300

    Fix workflow actions Android and iOS

---
+++ run_ci_android.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+adb install app.apk
+
+maestro test .maestro \
+  --include-tags=smoke \
+  --format junit \
+  --output report-android.xml \
+  -e MAESTRO_USERNAME="$MAESTRO_USERNAME" \
+  -e MAESTRO_PASSWORD="$MAESTRO_PASSWORD" \
+  -e MAESTRO_LOCKED_USERNAME="$MAESTRO_LOCKED_USERNAME" \
+  -e MAESTRO_LOCKED_PASSWORD="$MAESTRO_LOCKED_PASSWORD" \
+  -e MAESTRO_EMAIL_DOMAIN="$MAESTRO_EMAIL_DOMAIN" \
+  -e MAESTRO_CARD_NUMBER="$MAESTRO_CARD_NUMBER" \
+  -e MAESTRO_CARD_EXPIRATION="$MAESTRO_CARD_EXPIRATION" \
+  -e MAESTRO_CARD_CVV="$MAESTRO_CARD_CVV"
